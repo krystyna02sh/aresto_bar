948df7427c2f0d3ec8172fb35cbfe3ee8a28a028
Krystyna Shunkina <christina.shunkina@gmail.com>
2024-07-14 18:19:49 +0300

    hello world for render

---
+++ server/index.mjs
@@ -0,0 +1,25 @@
+import fastify from 'fastify';
+import fastifyStatic from '@fastify/static';
+import path from 'path';
+import { fileURLToPath } from 'url'
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const f = fastify();
+
+
+
+f.register(fastifyStatic, {
+    root: path.join(__dirname, '../client'),
+
+})
+const port = process.env.PORT || 5700;
+const host = process.env.HOST || 'localhost'
+f.listen({ port, host })
+    .then(() => {
+        console.log("Server is started")
+    })
+f.setNotFoundHandler((request, reply) => {
+    return reply.sendFile('index.html')
+})
